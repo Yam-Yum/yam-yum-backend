b4f0312c69e592526c2c1e32bc91ac7944e3496d
oa180 <oa20180180@gmail.com>
2024-05-31 02:59:24 +0300

    User test module just removed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { S3UploadModule } from './s3-upload/s3-upload.module';
 
 @Module({
-  imports: [
-    UsersModule,
-    ConfigModule.forRoot({ isGlobal: true }),
-    S3UploadModule,
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), S3UploadModule],
   controllers: [AppController],
   providers: [AppService],
 })
