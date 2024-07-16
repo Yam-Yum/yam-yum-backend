81f10c0bf77e19b040c5f467dbda0d6ed5e207d9
hassan-kamel <60411536+hassan-kamel@users.noreply.github.com>
2024-07-17 02:42:52 +0300

    fix(seeding): :ambulance: fix hashing password

--- src/database/seeds/user/user.factory.ts
+++ src/database/seeds/user/user.factory.ts
@@ -1,7 +1,8 @@
 import { User, UserGender, UserRole } from 'src/users/entities/user.entity';
 import { setSeederFactory } from 'typeorm-extension';
+import * as bcrypt from 'bcrypt';
 
-export default setSeederFactory(User, (faker) => {
+export default setSeederFactory(User, async (faker) => {
   const user = new User();
 
   user.firstName = faker.person.firstName();
@@ -9,7 +10,7 @@ export default setSeederFactory(User, (faker) => {
   user.username = faker.internet.userName();
   user.email = faker.internet.email();
   user.phoneNumber = faker.phone.number();
-  user.password = 'password';
+  user.password = await bcrypt.hash('password', 10);
   user.profilePicture = faker.image.avatar();
   user.role = faker.helpers.arrayElement([UserRole.ADMIN, UserRole.CHIEF, UserRole.ClIENT]);
   user.gender = faker.helpers.arrayElement([UserGender.MALE, UserGender.FEMALE]);
